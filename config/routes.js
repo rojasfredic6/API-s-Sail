cad9463391bc83369c1e076713142fa807834ef8
Fredic Yohany Rojas Parra <rojas.fredic6@gmail.com>
2021-07-13 18:41:39 -0500

    first commit

---
+++ config/routes.js
@@ -0,0 +1,15 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+module.exports.routes = {
+  // RUTAS DE USUARIO
+  "POST /user/login": "UserController.login",
+  "POST /user/signup": "UserController.signup",
+};
